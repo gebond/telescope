8c7fc091206630fd3ff9553e73fc8b7fa19d3b92
gbondarenko <gbondarenko@gds000057-nb.carina.griddynamics.net>
2017-10-28 09:38:06 +0400

    sockets ui + server

---
+++ server/src/main/java/org/hackday/telescope/WebSocketServer.java
@@ -0,0 +1,24 @@
+package org.hackday.telescope;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+
+/**
+ * Created on 28/10/17.
+ */
+public class WebSocketServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(MyWebSocketHandler.class);
+            }
+        };
+        server.setHandler(wsHandler);
+        server.start();
+        server.join();
+    }
+}
