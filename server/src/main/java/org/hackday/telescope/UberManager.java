8487390db68e2e94608ad963bcba7a1f754ccb82
gbondarenko <gbondarenko@gds000057-nb.carina.griddynamics.net>
2017-10-28 12:35:26 +0400

    add switch operations

--- server/src/main/java/org/hackday/telescope/UberManager.java
+++ server/src/main/java/org/hackday/telescope/UberManager.java
@@ -1,7 +1,10 @@
 package org.hackday.telescope;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jetty.websocket.api.Session;
+import org.hackday.telescope.commands.FuncType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +14,22 @@ import java.util.List;
  */
 public class UberManager {
     public static List<Session> activeSessions = new ArrayList<Session>();
-    public static ObjectMapper objectMapper = new ObjectMapper();
+    static JSONParser parser = new JSONParser();
 
-    public static String[] parseData(String message){
-
-        return message.split(message, ',');
+    public static String[] parseData(String message) throws ParseException {
+        Object obj = parser.parse(message);
+        JSONObject jsonObject = (JSONObject) obj;
+        FuncType funcType = FuncType.of((String) jsonObject.get(FuncType.getFuncTypeKey()));
+        switch (funcType) {
+            case GET_CHATS:
+                break;
+            case GET_MESSAGES:
+                break;
+            case SEND_MESSAGE:
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong funcType");
+        }
+        return null;
     }
 }
